3247a6ae0f7d0e30b1f4a1c60aa3786e705cc5e5
Chris Valmonte <jchrisvalmonte@gmail.com>
2017-12-30 23:36:24 -0800

    Updated attribution info

--- README.md
+++ README.md
@@ -4,6 +4,6 @@
 
  This work is licensed under the Creative Commons Attribution-NonCommercial 4.0 International License. To view a copy of this license, visit https://creativecommons.org/licenses/by-nc/4.0/.
 
- Based of Julian Garnier's original project, juliangarnier.com
+ Based off of Julian Garnier's original project, juliangarnier.com. The code from the original project has been modified here by Chris Valmonte.
 
  Copyright (c) 2016 Julian Garnier
--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,8 @@
  * This work is licensed under the Creative Commons Attribution-NonCommercial 4.0 International License. 
  * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc/4.0/.
  * 
- * Based off of Julian Garnier's original project, juliangarnier.com
+ * Based off of Julian Garnier's original project, juliangarnier.com. The code from the original
+ * project has been modified here by Chris Valmonte.
  * 
  * Copyright (c) 2016 Julian Garnier
  */
