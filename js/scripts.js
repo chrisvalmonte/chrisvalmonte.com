3140dc97951e3f571b4ef8f15036d7d66dfecfe9
Chris Valmonte <jchrisvalmonte@gmail.com>
2018-01-03 00:56:42 -0800

    Updated content

--- js/scripts.js
+++ js/scripts.js
@@ -39,8 +39,9 @@ var Messages = (function() {
   var _messages = [
     'Hi, I\'m Chris',
     'I design and develop user<br>interfaces for municipalities',
-    '<a href="https://behance.net/chrisvalmonte" target="_blank">behance.net/chrisvalmonte</a><br><a href="https://linkedin.com/in/chrisvalmonte" target="_blank">linkedin.com/in/chrisvalmonte</a><br><a href="https://instagram.com/chrisvalmonte.ig" target="_blank">instagram.com/chrisvalmonte.ig</a>',
+    'Want to get in touch?',
     'Say <a href="mailto:hello@chrisvalmonte.com">hello@chrisvalmonte.com</a>',
+    '<a href="https://behance.net/chrisvalmonte" target="_blank">behance.net/chrisvalmonte</a><br><a href="https://linkedin.com/in/chrisvalmonte" target="_blank">linkedin.com/in/chrisvalmonte</a><br><a href="https://instagram.com/chrisvalmonte.ig" target="_blank">instagram.com/chrisvalmonte.ig</a>',
     _getCurrentTimeMessage(),
   ]
 
