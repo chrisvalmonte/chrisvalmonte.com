8ac452f12bb3d0eb7a43d27e470c14d06af2f5fd
Chris Valmonte <jchrisvalmonte@gmail.com>
2018-01-02 23:01:00 -0800

    Replaced resume FAB with reply FAB. Updated content again...

--- js/scripts.js
+++ js/scripts.js
@@ -39,9 +39,8 @@ var Messages = (function() {
   var _messages = [
     'Hi, I\'m Chris',
     'I design and develop user<br>interfaces for municipalities',
-    'I enjoy traveling, playing video<br>games, and being a 12th man<br>for the Seattle Seahawks',
-    'Say <a href="mailto:hello@chrisvalmonte.com" target="_blank">hello@chrisvalmonte.com</a>',
     '<a href="https://behance.net/chrisvalmonte" target="_blank">behance.net/chrisvalmonte</a><br><a href="https://linkedin.com/in/chrisvalmonte" target="_blank">linkedin.com/in/chrisvalmonte</a><br><a href="https://instagram.com/chrisvalmonte.ig" target="_blank">instagram.com/chrisvalmonte.ig</a>',
+    'Say <a href="mailto:hello@chrisvalmonte.com">hello@chrisvalmonte.com</a>',
     _getCurrentTimeMessage(),
   ]
 
