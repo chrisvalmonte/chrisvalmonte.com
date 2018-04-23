d08f315b33b5c5e623b727566599029895a1b5b4
jchrisvalmonte <jchrisvalmonte@gmail.com>
2018-04-22 23:43:42 -0700

    feat: Various a11y fixes.
    - Move project aria-labels from the sections to the links.
    - On focus and hover, underline links in the navigation bar.
    - On focus and hover, color the navigation menu button.
    - Add aria-expanded attribute to the navigation drawer instead of the navigation menu button.
    - Style the navigation drawer based on the aria-expanded attribute (removed CSS noise from open class).

--- js/index.js
+++ js/index.js
@@ -1,8 +1,7 @@
 document
 .querySelector('#skip-link')
 .addEventListener('click', function(e) {
-	var drawer = document.querySelector('#nav-menu-drawer');
-	if(!drawer.classList.contains('open'))
+	if(!document.querySelector('#nav-menu-drawer').getAttribute('aria-expanded'))
 		return;
 
 	toggleNavMenuDrawer(e);
@@ -23,20 +22,20 @@ document
 
 function toggleNavMenuDrawer(event) {
 	var drawer = document.querySelector('#nav-menu-drawer');
-	drawer.classList.toggle('open');
+
 	drawer.querySelectorAll('.link').forEach(function(link) {
 		// Toggle tab index of link
 		link.getAttribute('tabindex') === '0'
 			? link.setAttribute('tabindex', '-1')
 			: link.setAttribute('tabindex', '0');
 	});
-	drawer.focus();
 
-	// Toggle ARIA expanded role of the navigation menu button
-	var navMenuBtn = document.querySelector('#nav-menu');
-	navMenuBtn.getAttribute('aria-expanded') === 'false'
-		? navMenuBtn.setAttribute('aria-expanded', 'true')
-		: navMenuBtn.setAttribute('aria-expanded', 'false');
+	// Toggle ARIA expanded role of the navigation menu drawer
+	drawer.getAttribute('aria-expanded') === 'false'
+		? drawer.setAttribute('aria-expanded', 'true')
+		: drawer.setAttribute('aria-expanded', 'false');
+
+	drawer.focus();
 
 	document
 	.querySelectorAll('.hide-from-drawer')
